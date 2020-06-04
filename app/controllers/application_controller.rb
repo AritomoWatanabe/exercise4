0a541f0c1db3018975d3f42a4e068ccd066c32f5
渡辺有智 <aritomo@aritomo.local>
2020-06-04 20:37:08 +0900

    start

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < ActionController::Base
+	# before_action :authenticate_user!
+	protect_from_forgery with: :exception
+
+
+  def after_sign_in_path_for(resource)
+     user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+
+  # deviseコントローラーにストロングパラメータを追加する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    #sign_upの際にnameのデータ操作を許。追加したカラム。
+    #サインイン時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
+end
