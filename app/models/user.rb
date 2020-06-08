be06cb51b9f7d1ddcab2235dc89dbd831a057e9b
渡辺有智 <aritomo@aritomo.local>
2020-06-08 22:51:22 +0900

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,16 @@ class User < ApplicationRecord
 
   attachment :profile_image, destroy: false
 
-  #follower_idを使ってuserと結びつけをするが、followerクラスはないため、クラス名と結びつけるforeign_keyを定義する
   has_many :active_relationships, class_name: "Relationship",
                     foreign_key: "follower_id",
                     dependent: :destroy
+  #user.followedsという名は不適切なのでfollowingにする
+  has_many :following, through: :active_relationships, source: :followed
+
 
   has_many :passive_relationships, class_name: "Relationship",
                     foreign_key: "followed_id",
                     dependent: :destroy
-
-  #user.followedsという名は不適切なのでfollowingにする
-  has_many :following, through: :active_relationships, source: :followed
 
   has_many :followers, through: :passive_relationships, source: :follower
 
